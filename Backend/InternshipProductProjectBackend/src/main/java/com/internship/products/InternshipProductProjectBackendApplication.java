d838a406c08aed7b275f8328b5e0667264c1095a
Tsvetelin Kostadinov <tsvetelinkostadinovts@gmail.com>
2019-05-21 10:53:54 +0300

    Initial commit

---
+++ Backend/InternshipProductProjectBackend/src/main/java/com/internship/products/InternshipProductProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.internship.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipProductProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipProductProjectBackendApplication.class, args);
+	}
+
+}
---
+++ Backend/InternshipProductProjectBackend/src/test/java/com/internship/products/InternshipProductProjectBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.internship.products;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InternshipProductProjectBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
